d7160a0c637f180cd66bca80f0650e6d81a36c8f
yoshida-kadai-fallsemester <seiryu121020@gmail.com>
2022-01-19 13:01:17 +0000

    課題９を提出

---
+++ kadai-09/main.js
@@ -0,0 +1,1 @@
+(()=>{var o={902:o=>{o.exports=()=>{console.log("hello, A!")}},78:o=>{o.exports=()=>{console.log("hello, B!")}}},r={};function e(t){var s=r[t];if(void 0!==s)return s.exports;var l=r[t]={exports:{}};return o[t](l,l.exports,e),l.exports}(()=>{const o=e(902),r=e(78);o(),r()})()})();
